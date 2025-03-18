0140ebad10cb20c8a8281c224691beeaf9c652af
Richard Latak (SPG) <yahzee@gmx.at>
2025-03-18 16:29:03 +0100

    HÜ 13.03.2025

---
+++ HÜ_03_13/main.js
@@ -0,0 +1,7 @@
+// **HÜ** in `main.js` ein anderes Objekt servieren
+//
+//- in `index.html` mache einen button welcher den Endpoint holt(fetch) und das Json
+//in eine textarea hineinschreibt!
+//
+//Abgabe bis 19.3. 13h
+//////////////////////////////////////////////////////////////////////
